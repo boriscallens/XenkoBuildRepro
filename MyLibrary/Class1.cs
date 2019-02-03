f4fc6a27db9d1c4bf300eb10bca4c997128d2b88
Boris Callens <boris.Callens+github@gmail.com>
2019-02-03 15:08:04 +0100

    new super default game sln with separate code library

---
+++ MyLibrary/Class1.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Xenko.Core.Mathematics;
+using Xenko;
+using Xenko.Rendering;
+using Xenko.Engine;
+using Xenko.Graphics;
+
+namespace MyLibrary
+{
+    public class Class1
+    {
+        public void Method1()
+        {
+
+        }
+    }
+}
